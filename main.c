428874aa17807e243561cbbd86cef3cf20819071
Leandree <carpentier.leandre@gmail.com>
2019-01-26 16:16:42 +0100

    aze

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+
+int main(){
+
+	system("mkdir -p /tmp/kahoot");
+
+	int fd;
+    char * myfifo = "/tmp/kahoot/myfifo";
+
+    /* create the FIFO (named pipe) */
+    mkfifo(myfifo, 0666);
+
+    /* write "Hi" to the FIFO */
+    fd = open(myfifo, O_WRONLY);
+    write(fd, "Hi", sizeof("Hi"));
+    close(fd);
+
+    /* remove the FIFO */
+    unlink(myfifo);
+
+    return 0;
+
+}
